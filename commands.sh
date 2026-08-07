107acdec0494eb0bcc4910f0a38f71e183bf8307
acidbytes.h <gmdsagittarius@gmail.com>
2026-08-08 00:16:31 +0100

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,5 @@
+# Here are the commands you have to run to get the best experience out of my .bashrc! (Only for Manjaro and Arch currently)
+
+sudo pacman -Syu figlet
+sudo pacman -Syu fastfetch
+sudo pacman -Syu lolcat
